c442c621dde95d5e7535fa5a295b15625852f60e
Kamilahm1 <kamilahmorgan1@gmail.com>
2018-07-17 21:22:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+  get '/name' do
+    "My name is Kamilah"
+  end
+  get '/hometown' do
+    "My hometown is Los Angeles"
+  end
+  get '/favorite-song' do
+    "My favorite song is Spotless Mind"
+  end
+  end
+
+
